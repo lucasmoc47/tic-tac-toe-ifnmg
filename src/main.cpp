b8fff4acacdad9cf95327ab633a46cc14ead3c77
Lucas Ribeiro <lucas.sr.wc@gmail.com>
2014-12-11 15:45:24 -0200

    header fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+#ifdef linux
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 #include <cstdio>
+#endif
+
+#ifdef _WIN32
+#include <SDL.h>
+#include <SSDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <cstdio>
+#endif
 
 const int SCREEN_WIDTH = 311;
 const int SCREEN_HEIGHT = 308;
